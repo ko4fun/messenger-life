5d8ec614b42a0ac142d4598429b9e3496d7b84f9
ko4fun <53464336+ko4fun@users.noreply.github.com>
2026-03-11 18:52:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 const io     = new Server(server, { cors: { origin: '*' } });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname)));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
